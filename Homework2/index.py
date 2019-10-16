6f87ccb4a7a966355a07b690c38a3f3a450ec997
Keerti Bhogaraju <54336013+keerti-1997@users.noreply.github.com>
2019-10-15 23:45:14 -0700

    Updated index.py
    Index doesn't have repeated keywords

--- Homework2/index.py
+++ Homework2/index.py
@@ -4,105 +4,71 @@ import string
 import time
 
 
-avoidable = string.punctuation
-avoidable = avoidable.replace("'","")
-stop_words = ['a','an','the','of','to','as','only','he','she','it','they','his','her','and','are','into']
-
 def printf(elements):
 	for element in elements:
 		print(etree.tostring(element, pretty_print = True))
 
-# def make_elements(new_root,split_words,child_no):
-# 	if child_no == 2:
-
-# 	for keyword in split_words: 
-# 		child = etree.SubElement(new_root,"keyword") 
-# 		child.text = keyword
-# 		sub_child = etree.SubElement(child, "id")
-# 		sub_child.text = element.get("id")
-# 		another_child = etree.SubElement(child,str(element.getchildren()[child_no].tag))
-# 		another_child.text = element.getchildren()[child_no].text
-# 		return new_root
-
+avoidable = string.punctuation
+avoidable = avoidable.replace("'","")
 f_in = open(sys.argv[1])
-#f_out = open(sys.argv[2])
-
 tree = etree.parse(f_in)
-#print(etree.tostring(tree, pretty_print = True))
-
 root = tree.getroot()
-new_root = etree.Element("keywords_catalog")
+all_keywords = {}
 
 # author
 for element in root.iter("book"):
-	#print(element.tag, element.attrib)
-	#print(element.getchildren()[0].text)
 	split_words = element.getchildren()[0].text.lower().split(", ")
-	#print(split_words)
 	for keyword in split_words: 
-		child = etree.SubElement(new_root,"keyword") 
-		child.text = keyword
-		child.set("id",element.get("id"))
-		child.set(str(element.getchildren()[1].tag),element.getchildren()[1].text)
-		# sub_child = etree.SubElement(child, "id")
-		# sub_child.text = element.get("id")
-		# another_child = etree.SubElement(child,str(element.getchildren()[0].tag))
-		# another_child.text = element.getchildren()[0].text
-	#new_root = make_elements(new_root,split_words,0)
+		if keyword != '\n' and keyword not in all_keywords:
+			all_keywords[keyword] = {}
+		all_keywords[keyword][element.get("id")] = {} 
+		all_keywords[keyword][element.get("id")][element.getchildren()[0].tag] = element.getchildren()[0].text
 
 # title
 for element in root.iter("book"):
-	#print(element.getchildren()[1].text)
 	word = element.getchildren()[1].text.lower()
 	for ch in avoidable:
 		word = word.replace(ch,"")
 	split_words = word.split(" ")
-	#print(split_words)	
 	for keyword in split_words:
-		child = etree.SubElement(new_root,"keyword")
-		child.text = keyword
-		child.set("id",element.get("id"))
-		child.set(str(element.getchildren()[1].tag),element.getchildren()[1].text)
-		#sub_child = etree.SubElement(child, "id")
-		#sub_child.text = element.get("id")
-		#another_child = etree.SubElement(child,str(element.getchildren()[1].tag))
-		#another_child.text = element.getchildren()[1].text
-	#new_root = make_elements(new_root,split_words,1)
+		if keyword != '\n' and keyword not in all_keywords:
+			all_keywords[keyword] = {}
+		all_keywords[keyword][element.get("id")] = {} 
+		all_keywords[keyword][element.get("id")][element.getchildren()[1].tag] = element.getchildren()[1].text
 
 
 # genre
 for element in root.iter("book"):
 	split_words = element.getchildren()[2].text.lower() 
-	#print(split_words)	
-	#new_root = make_elements(new_root,split_words,2)
-	child = etree.SubElement(new_root,"keyword")
-	child.text = split_words
-	child.set("id",element.get("id"))
-	child.set(str(element.getchildren()[1].tag),element.getchildren()[1].text)
-	# sub_child = etree.SubElement(child, "id")
-	# sub_child.text = element.get("id")
-	# another_child = etree.SubElement(child,str(element.getchildren()[2].tag))
-	# another_child.text = element.getchildren()[2].text
+	if split_words != '\n' and split_words not in all_keywords:
+		all_keywords[keyword] = {}
+	all_keywords[keyword][element.get("id")] = {} 
+	all_keywords[keyword][element.get("id")][element.getchildren()[2].tag] = element.getchildren()[2].text
 
 # description
 for element in root.iter("book"):
-	#print(element.getchildren()[5].text)
 	sentence = element.getchildren()[5].text.lower().replace("\n","").replace("  ","")
-	#print(sentence)
 	for ch in avoidable:
 		sentence = sentence.replace(ch," ")
 	split_words = sentence.split(" ")
-	#print(split_words)	
-	#new_root = make_elements(new_root,split_words,5)
 	for keyword in split_words:
-		if keyword != '':
-			child = etree.SubElement(new_root,"keyword")
-			child.text = keyword
-			# sub_child = etree.SubElement(child, "id")
-			# sub_child.text = element.get("id")
-			# another_child = etree.SubElement(child,str(element.getchildren()[5].tag))
-			# another_child.text = element.getchildren()[5].text
-			child.set("id",element.get("id"))
-			child.set(str(element.getchildren()[1].tag),element.getchildren()[1].text)		
+		if keyword != '' and keyword != '\n':
+			if keyword not in all_keywords:
+				all_keywords[keyword] = {}
+			all_keywords[keyword][element.get("id")] = {} 
+			all_keywords[keyword][element.get("id")][element.getchildren()[5].tag] = element.getchildren()[5].text
+
+new_root = etree.Element("keywords_catalog") 
+for key, val in all_keywords.items(): 
+	keyword = etree.SubElement(new_root,"keyword") 
+	keyword.text = key 
+	for k,v in val.items(): 
+		ids = etree.SubElement(keyword,"id") 
+		ids.text = k 
+		for a_k, a_v in v.items(): 
+			attr = etree.SubElement(ids,a_k) 
+			attr.text = a_v 
+
+
 et = etree.ElementTree(new_root)
 et.write(sys.argv[2],pretty_print=True)
